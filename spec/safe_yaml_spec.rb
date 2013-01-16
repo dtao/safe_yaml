7c53fef8f2c75a14baf28e89dbbcd3181cd50434
Dan Tao <daniel.tao@gmail.com>
2013-01-16 12:42:34 -0800

    added specs for exploits using #init_with

--- spec/safe_yaml_spec.rb
+++ spec/safe_yaml_spec.rb
@@ -4,14 +4,28 @@ require "safe_yaml"
 require "exploitable_back_door"
 
 describe YAML do
+  before :each do
+    ExploitableBackDoor.reset
+  end
+
   describe "load" do
     it "allows exploits through objects defined in YAML w/ !ruby/hash" do
       YAML.load "--- !ruby/hash:ExploitableBackDoor\nfoo: bar\n"
       ExploitableBackDoor.should be_exploited
     end
+
+    it "allows exploits through objects defined in YAML w/ !ruby/object" do
+      YAML.load "--- !ruby/object:ExploitableBackDoor\nfoo: bar\n"
+      ExploitableBackDoor.should be_exploited
+    end
   end
 
   describe "safe_load" do
+    it "does NOT allow exploits through objects defined in YAML w/ !ruby/object" do
+      YAML.safe_load "--- !ruby/object:ExploitableBackDoor\nfoo: bar\n"
+      ExploitableBackDoor.should_not be_exploited
+    end
+
     it "does NOT allow exploits through objects defined in YAML w/ !ruby/hash" do
       YAML.safe_load "--- !ruby/hash:ExploitableBackDoor\nfoo: bar\n"
       ExploitableBackDoor.should_not be_exploited
