3c3ed0e0f63e22dc1960413cf1a1e572c29135d4
Dan Tao <daniel.tao@gmail.com>
2013-01-17 21:25:02 -0800

    removed some unneeded dependencies

--- Gemfile
+++ Gemfile
@@ -4,7 +4,6 @@ gemspec
 
 group :development do
   gem "heredoc_unindent"
-  gem "pry"
   gem "rake"
   gem "rspec"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,5 @@ $LOAD_PATH << File.join(ROOT, "lib")
 $LOAD_PATH << File.join(HERE, "support")
 
 require "heredoc_unindent"
-require "pry"
 
 require File.join(HERE, "shared_specs")
