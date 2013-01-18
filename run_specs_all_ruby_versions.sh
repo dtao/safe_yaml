135e3a4b96c7c5d2f6f9b1cf1060394d323be943
Dan Tao <daniel.tao@gmail.com>
2013-01-17 21:27:37 -0800

    added a script to run specs for all supported Ruby versions (1.8.7, 1.9.2, 1.9.3)

---
+++ run_specs_all_ruby_versions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
+
+rvm use 1.8.7@safe_yaml
+rake spec
+
+rvm use 1.9.2@safe_yaml
+rake spec
+
+rvm use 1.9.3@safe_yaml
+rake spec
