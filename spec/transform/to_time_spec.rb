5ce3a0501c5c98dbb640a81db451d62fb28747d1
Dan Tao <daniel.tao@gmail.com>
2013-02-01 18:14:32 -0800

    fixed spec for Ruby 1.8.7

--- spec/transform/to_time_spec.rb
+++ spec/transform/to_time_spec.rb
@@ -1,8 +1,11 @@
 require File.join(File.dirname(__FILE__), "..", "spec_helper")
 
 describe SafeYAML::Transform::ToTime do
-  it "returns true when the value matches a valid Time" do
-    subject.transform?("2013-01-01 10:00:00")[0].should == true
+  # YAML don't parse times prior to 1.9.
+  if RUBY_VERSION >= "1.9.2"
+    it "returns true when the value matches a valid Time" do
+      subject.transform?("2013-01-01 10:00:00")[0].should == true
+    end
   end
 
   it "returns false when the value does not match a valid Time" do
