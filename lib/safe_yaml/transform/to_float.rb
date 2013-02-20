d2e2dd6044de3e5c07d7e66149e3614c681699da
Dan Tao <daniel.tao@gmail.com>
2013-02-20 11:32:13 -0800

    brought parsing of floating point numbers into better compliance w/ YAML spec - see #26

--- lib/safe_yaml/transform/to_float.rb
+++ lib/safe_yaml/transform/to_float.rb
@@ -1,11 +1,48 @@
 module SafeYAML
   class Transform
     class ToFloat
-      MATCHER = /\A\d*\.\d+\Z/.freeze
+      Infinity = 1.0 / 0.0
+      NaN = 0.0 / 0.0
+
+      PREDEFINED_VALUES = {
+        ".inf"  => Infinity,
+        ".Inf"  => Infinity,
+        ".INF"  => Infinity,
+        "-.inf" => -Infinity,
+        "-.Inf" => -Infinity,
+        "-.INF" => -Infinity,
+        ".nan"  => NaN,
+        ".NaN"  => NaN,
+        ".NAN"  => NaN,
+      }.freeze
+
+      SEXAGESIMAL_MATCHER = /\A[-+]?[0-9][0-9_]*(:[0-5]?[0-9])+\.[0-9_]*\Z/.freeze
 
       def transform?(value)
-        return false unless MATCHER.match(value)
-        return true, value.to_f
+        return true, Float(value) rescue try_edge_cases?(value)
+      end
+
+      def try_edge_cases?(value)
+        return true, PREDEFINED_VALUES[value] if PREDEFINED_VALUES.include?(value)
+        return true, parse_sexagesimal(value) if SEXAGESIMAL_MATCHER.match(value)
+        return false
+      end
+
+      def parse_sexagesimal(value)
+        before_decimal, after_decimal = value.split(".")
+
+        whole_part = 0
+        multiplier = 1
+
+        before_decimal = before_decimal.split(":")
+        until before_decimal.empty?
+          whole_part += (Float(before_decimal.pop) * multiplier)
+          multiplier *= 60
+        end
+
+        result = whole_part + Float("." + after_decimal)
+        result *= -1 if value[0] == "-"
+        result
       end
     end
   end
