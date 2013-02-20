97611f817e316c22f3783daa1bac1a7d29ac84d7
Dan Tao <daniel.tao@gmail.com>
2013-02-20 07:03:02 -0800

    fixed parsing of octal and hexadecimal numbers - fixes #25

--- lib/safe_yaml/transform/to_integer.rb
+++ lib/safe_yaml/transform/to_integer.rb
@@ -1,9 +1,15 @@
 module SafeYAML
   class Transform
     class ToInteger
-      MATCHER = /\A\d+\Z/.freeze
+      OCTAL_MATCHER = /\A0[0-7]+\Z/.freeze
+      HEXADECIMAL_MATCHER = /\A0x[0-9a-f]+\Z/i.freeze
+      MATCHER = /\A[1-9]\d*\Z/.freeze
 
       def transform?(value)
+        if OCTAL_MATCHER.match(value) || HEXADECIMAL_MATCHER.match(value)
+          return true, Integer(value)
+        end
+
         return false unless MATCHER.match(value)
         return true, value.to_i
       end
--- spec/transform/to_integer_spec.rb
+++ spec/transform/to_integer_spec.rb
@@ -2,7 +2,7 @@ require File.join(File.dirname(__FILE__), "..", "spec_helper")
 
 describe SafeYAML::Transform::ToInteger do
   it "returns true when the value matches a valid Integer" do
-    subject.transform?("10")[0].should be_true
+    subject.transform?("10").should be_true
   end
 
   it "returns false when the value does not match a valid Integer" do
@@ -11,5 +11,21 @@ describe SafeYAML::Transform::ToInteger do
 
   it "returns false when the value spans multiple lines" do
     subject.transform?("10\nNOT AN INTEGER").should be_false
+  end
+
+  it "correctly parses numbers in octal format" do
+    subject.transform?("010").should == [true, 8]
+  end
+
+  it "correctly parses numbers in hexadecimal format" do
+    subject.transform?("0x1FF").should == [true, 511]
+  end
+
+  it "defaults to a string for a number that resembles octal format but is not" do
+    subject.transform?("09").should be_false
+  end
+
+  it "defaults to a string for a number that resembles hexadecimal format but is not" do
+    subject.transform?("0x1G").should be_false
   end
 end
