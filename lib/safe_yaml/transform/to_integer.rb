4b85203349e818603b833d21dd498d1abcec78bc
Dan Tao <daniel.tao@gmail.com>
2013-02-20 10:49:10 -0800

    simplified implementation of Transform::ToInteger

--- lib/safe_yaml/transform/to_integer.rb
+++ lib/safe_yaml/transform/to_integer.rb
@@ -1,17 +1,8 @@
 module SafeYAML
   class Transform
     class ToInteger
-      OCTAL_MATCHER = /\A0[0-7]+\Z/.freeze
-      HEXADECIMAL_MATCHER = /\A0x[0-9a-f]+\Z/i.freeze
-      MATCHER = /\A[1-9]\d*\Z/.freeze
-
       def transform?(value)
-        if OCTAL_MATCHER.match(value) || HEXADECIMAL_MATCHER.match(value)
-          return true, Integer(value)
-        end
-
-        return false unless MATCHER.match(value)
-        return true, value.to_i
+        return true, Integer(value) rescue false
       end
     end
   end
