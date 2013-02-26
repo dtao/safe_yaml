2e4ae78d54ae66608bf0d4ecd08bc26d459cb8f7
Steven Davidovitz <steven.davidovitz@gmail.com>
2013-02-25 19:48:33 -0800

    base64 test fix for 1.8

--- spec/transform/base64_spec.rb
+++ spec/transform/base64_spec.rb
@@ -3,6 +3,9 @@ require File.join(File.dirname(__FILE__), "..", "spec_helper")
 describe SafeYAML::Transform do
   it "should return the same encoding when decoding Base64" do
     value = "c3VyZS4="
-    SafeYAML::Transform.to_proper_type(value, false, "!binary").encoding.should == value.encoding
+    decoded = SafeYAML::Transform.to_proper_type(value, false, "!binary")
+
+    decoded.should == "sure."
+    decoded.encoding.should == value.encoding if decoded.respond_to?(:encoding)
   end
 end
