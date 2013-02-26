33606a8751ce2aa375a1c6b36136b5df4dfc768d
Dan Tao <daniel.tao@gmail.com>
2013-02-25 20:53:01 -0800

    fixed a small mistake in the script to run specs for all (well, most) supported Ruby versions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,8 @@ ROOT = File.join(HERE, "..") unless defined?(ROOT)
 $LOAD_PATH << File.join(ROOT, "lib")
 $LOAD_PATH << File.join(HERE, "support")
 
+require "yaml"
 if ENV["YAMLER"] && defined?(YAML::ENGINE)
-  require "yaml"
   YAML::ENGINE.yamler = ENV["YAMLER"]
   puts "Running specs in Ruby #{RUBY_VERSION} with '#{YAML::ENGINE.yamler}' YAML engine."
 end
