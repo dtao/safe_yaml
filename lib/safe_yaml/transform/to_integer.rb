4fbdfecd195c80024e2bab4296ab5055daa7ff5e
Dan Tao <daniel.tao@gmail.com>
2013-04-29 10:31:57 -0700

    well that was a botched merged

--- lib/safe_yaml/transform/to_integer.rb
+++ lib/safe_yaml/transform/to_integer.rb
@@ -2,7 +2,7 @@ module SafeYAML
   class Transform
     class ToInteger
       MATCHERS = Deep.freeze([
-        /\A\s*[-+]?[1-9][0-9_]*\s*\Z/, # decimal
+        /\A\s*[-+]?[1-9][0-9_,]*\s*\Z/, # decimal
         /\A\s*0[0-7]+\s*\Z/,           # octal
         /\A\s*0x[0-9a-f]+\s*\Z/i,      # hexadecimal
         /\A\s*0b[01_]+\s*\Z/           # binary
@@ -10,7 +10,7 @@ module SafeYAML
 
       def transform?(value)
         MATCHERS.each do |matcher|
-          return true, Integer(value) if matcher.match(value)
+          return true, Integer(value.gsub(",", "")) if matcher.match(value)
         end
         try_edge_cases?(value)
       end
