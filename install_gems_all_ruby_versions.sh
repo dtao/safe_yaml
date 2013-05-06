c8ebe5bc06ba68fe17e6c72e90425e82e04dc131
Dan Tao <daniel.tao@gmail.com>
2013-05-06 10:06:49 -0700

    definitely don't need to specify YAMLER for bundle install

--- install_gems_all_ruby_versions.sh
+++ install_gems_all_ruby_versions.sh
@@ -6,10 +6,10 @@ rvm use 1.8.7@safe_yaml
 bundle install
 
 rvm use 1.9.2@safe_yaml
-YAMLER=syck bundle install
+bundle install
 
 rvm use 1.9.3@safe_yaml
-YAMLER=syck bundle install
+bundle install
 
 rvm use 2.0.0@safe_yaml
-YAMLER=psych bundle install
+bundle install
