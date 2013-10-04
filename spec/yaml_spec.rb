86bf7cfdf451a356c208c9002049e4eafa32e464
Dan Tao <daniel.tao@gmail.com>
2013-10-04 16:55:38 -0700

    typo

--- spec/yaml_spec.rb
+++ spec/yaml_spec.rb
@@ -3,7 +3,7 @@
 require File.join(File.dirname(__FILE__), "spec_helper")
 
 describe YAML do
-  context "when you've only required safe_yaml/safe_load", :libraries => true do
+  context "when you've only required safe_yaml/load", :libraries => true do
     it "YAML.load doesn't get monkey patched" do
       YAML.method(:load).should == ORIGINAL_YAML_LOAD
     end
