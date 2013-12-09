677b26fd85025b2724c2c201a15f6cd91e5c59ea
Dan Tao <daniel.tao@gmail.com>
2013-12-09 09:33:32 -0800

    updated spec script to use bundle exec instead of gemsets

--- run_specs_all_ruby_versions.sh
+++ run_specs_all_ruby_versions.sh
@@ -2,26 +2,26 @@
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 
-rvm use 1.8.7@safe_yaml
-rake spec
+rvm use 1.8.7
+bundle exec rake spec
 
-rvm use 1.9.2@safe_yaml
-YAMLER=syck rake spec
+rvm use 1.9.2
+YAMLER=syck bundle exec rake spec
 
-rvm use 1.9.3@safe_yaml
-YAMLER=syck rake spec
+rvm use 1.9.3
+YAMLER=syck bundle exec rake spec
 
-rvm use 1.9.2@safe_yaml
-YAMLER=psych rake spec
+rvm use 1.9.2
+YAMLER=psych bundle exec rake spec
 
-rvm use 1.9.3@safe_yaml
-YAMLER=psych rake spec
+rvm use 1.9.3
+YAMLER=psych bundle exec rake spec
 
-rvm use 2.0.0@safe_yaml
-YAMLER=psych rake spec
+rvm use 2.0.0
+YAMLER=psych bundle exec rake spec
 
-rvm use jruby@safe_yaml
-JRUBY_OPTS=--1.8 rake spec
+rvm use jruby
+JRUBY_OPTS=--1.8 bundle exec rake spec
 
-rvm use jruby@safe_yaml
-JRUBY_OPTS=--1.9 rake spec
+rvm use jruby
+JRUBY_OPTS=--1.9 bundle exec rake spec
