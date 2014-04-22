455e1ca4a9331b63fa91a3a4e7ee84fa0d92189b
Dan Tao <daniel.tao@gmail.com>
2014-04-22 08:26:19 -0700

    getting rid of libyaml check (see #56, #57, and #60)

--- lib/safe_yaml/load.rb
+++ lib/safe_yaml/load.rb
@@ -7,10 +7,6 @@ module SafeYAML
   YAML_ENGINE = defined?(YAML::ENGINE) ? YAML::ENGINE.yamler : "syck"
 end
 
-require "safe_yaml/libyaml_checker"
-
-SafeYAML::LibyamlChecker.check_libyaml_version
-
 require "safe_yaml/deep"
 require "safe_yaml/parse/hexadecimal"
 require "safe_yaml/parse/sexagesimal"
