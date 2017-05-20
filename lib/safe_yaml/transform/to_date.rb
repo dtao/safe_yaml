2f6d98f300fe76ef9222710625473567a6ea669a
Tobias Brink <tobias.brink@gmail.com>
2017-05-20 16:29:08 +0200

    Optionally preserve timezone of parsed dates.

--- lib/safe_yaml/transform/to_date.rb
+++ lib/safe_yaml/transform/to_date.rb
@@ -1,9 +1,9 @@
 module SafeYAML
   class Transform
     class ToDate
-      def transform?(value)
+      def transform?(value, options=SafeYAML::OPTIONS)
         return true, Date.parse(value) if Parse::Date::DATE_MATCHER.match(value)
-        return true, Parse::Date.value(value) if Parse::Date::TIME_MATCHER.match(value)
+        return true, Parse::Date.value(value, options[:preserve_timezone]) if Parse::Date::TIME_MATCHER.match(value)
         false
       rescue ArgumentError
         return true, value
